7e553335bd78c5a3acc6e744799d2a5cc90908e2
Mathias DIDIER <mathias.didier@consertotech.pro>
2018-05-09 14:51:42 +0200

    end try inputHelper

--- Source/PocUE4/Private/HMD_Detector_Actor.cpp
+++ Source/PocUE4/Private/HMD_Detector_Actor.cpp
@@ -2,7 +2,8 @@
 
 #include "HMD_Detector_Actor.h"
 #include "Engine.h"
-
+#include "HeadMountedDisplayTypes.h"
+#include "IXRTrackingSystem.h"
 
 // Sets default values
 AHMD_Detector_Actor::AHMD_Detector_Actor()
@@ -28,7 +29,24 @@ void AHMD_Detector_Actor::Tick(float DeltaTime)
 void AHMD_Detector_Actor::GetDeviceName()
 {
 	//GEngine->AddOnScreenDebugMessage(-1, 50, FColor::Red, "debug msg", true, FVector2D(1000.f,1000.f));
-	UE_LOG(LogTemp, Warning, TEXT("Your message"));
+	UE_LOG(LogTemp, Warning, TEXT("--Begin get device name"));
+	
+	if (GEngine->XRSystem.IsValid()) //check pointer is valid
+	{
+		
+		//EHMDDeviceType::Type  device = 
+		IHeadMountedDisplay* device = GEngine->XRSystem->GetHMDDevice();
+		
+
+			/*
+		if (result == EHMDDeviceType::DT_OculusRift) UE_LOG(LogTemp, Warning, TEXT("Device is Rift"));
+		else if (result == EHMDDeviceType::DT_GearVR) UE_LOG(LogTemp, Warning, TEXT("Device is GearVR"));
+		else UE_LOG(LogTemp, Warning, TEXT("Device is Other"));*/
+
+	}
+	else UE_LOG(LogTemp, Error, TEXT("No device found!"));
+
+	UE_LOG(LogTemp, Warning, TEXT("--End get device name"));
 	//UGameplayStatics::OpenLevel()
 }
 
